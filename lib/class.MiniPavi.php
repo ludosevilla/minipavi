4357050d1e857305fb2fe200f021ffb5674bd736
Jean-arthur Silve <150791030+ludosevilla@users.noreply.github.com>
2025-10-12 19:20:49 +0200

    Add files via upload

--- lib/class.MiniPavi.php
+++ lib/class.MiniPavi.php
@@ -15,6 +15,7 @@
  * 13/09/2024 : ajout fonctions pour l'économiseur d'écran
  * 11/10/2024 : ajout addCommandInputForm
  * 08/07/2025 : ajout addCommandSetParam
+ * 12/10/2025 : ajout de la possibilité de champs "mot de passe" dans la saisie de formulaires (addCommandInputForm)
  */
 
 
@@ -502,6 +503,10 @@ class MiniPavi {
 				case 'InputTxt':
 					if ($this->command->param->char!='')
 						$altChar=$this->command->param->char;
+					break;
+				case 'InputForm':
+					if (isset($this->command->param->char) && isset($this->command->param->char[$this->bufferIdx]) && $this->command->param->char[$this->bufferIdx]!='')
+						$altChar=$this->command->param->char[$this->bufferIdx];
 					break;
 				default:
 					break;
@@ -1399,7 +1404,9 @@ class MiniPavi {
 				$this->currX=$this->command->param->x[$this->bufferIdx];
 				$this->count=0;
 			}
-			$vdt = self::getVdtPos($this->command->param->x[$this->bufferIdx],$this->currY).$this->buffer[$this->bufferIdx];
+			if (isset($this->command->param->char) && isset($this->command->param->char[$this->bufferIdx]) && $this->command->param->char[$this->bufferIdx]!='') {
+				$vdt = self::getVdtPos($this->command->param->x[$this->bufferIdx],$this->currY).str_repeat($this->command->param->char[$this->bufferIdx], $this->count);
+			} else 	$vdt = self::getVdtPos($this->command->param->x[$this->bufferIdx],$this->currY).$this->buffer[$this->bufferIdx];
 			return $vdt;
 		}
 	}
